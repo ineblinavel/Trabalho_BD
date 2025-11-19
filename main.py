16417c428b739e9dc6248daa108d5ac59b39ce49
JoaoG0988 <gabrielmachado.6677@gmail.com>
2025-11-19 09:06:59 -0300

    testando find_by_crm

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ def main():
         print(f"CRM: {medico['crm']}, Nome: {medico['nome_medico']}, CPF: {medico['cpf']}, Salário: {medico['salario']}")
 
 
+    medico_bycrm = medico_repo.find_by_crm("123456")
+    if medico_bycrm:
+        print("\n--- MÉDICO ENCONTRADO POR CRM ---")
+        print(f"CRM: {medico_bycrm['crm']}, Nome: {medico_bycrm['nome_medico']}, CPF: {medico_bycrm['cpf']}, Salário: {medico_bycrm['salario']}")
+    else:
+        print("\nMédico com CRM 123456 não encontrado.")
+
 
 
 if __name__ == "__main__":
